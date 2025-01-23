56ae7607d30538bfbef030e73d59dec1abe6eb20
Kulikov_Roman <kulrom.otr.rarus@gmail.com>
2025-01-23 21:33:22 +0300

    Initial commit

---
+++ tools/src/main/java/com/roman_kulikov/tools/base/EdgeToEdgeProp.kt
@@ -0,0 +1,12 @@
+package com.roman_kulikov.tools.base
+
+import androidx.activity.SystemBarStyle
+import androidx.activity.enableEdgeToEdge
+
+/**
+ * EdgeToEdgeProp contains param for set [enableEdgeToEdge] Activity function.
+ */
+data class EdgeToEdgeProp(
+    val statusBarStyle: SystemBarStyle? = null,
+    val navigationBarStyle: SystemBarStyle? = null
+)
---
+++ tools/src/main/java/com/roman_kulikov/tools/base/ScreenSettings.kt
@@ -0,0 +1,18 @@
+package com.roman_kulikov.tools.base
+
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.runtime.Composable
+import com.roman_kulikov.tools.base.EdgeToEdgeProp
+
+
+/**
+ * ScreenSettings contains:
+ * @param edgeToEdgeEnabling -> Boolean type for enable [enableEdgeToEdge].
+ * @param edgeProp -> [EdgeToEdgeProp] contain properties like in original [enableEdgeToEdge].
+ * @param content -> This is the composable content of the screen.
+ */
+data class ScreenSettings(
+    var edgeToEdgeEnabling: Boolean = false,
+    var edgeProp: EdgeToEdgeProp? = null,
+    var content: @Composable () -> Unit = { /* nothing */ }
+)
